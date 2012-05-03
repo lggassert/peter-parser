57b165569e4cbab9328d506c19b2228f6610fb20
Lucas G Gassert <lggassert@gmail.com>
2012-05-03 05:51:28 -0300

    "Fixed wrong require"

--- peter-parser.rb
+++ peter-parser.rb
@@ -3,4 +3,4 @@ require 'nokogiri'
 
 require_relative './peter-parser/errors.rb'
 require_relative './peter-parser/parser.rb'
-require_relative './peter-parser/xml_parser.rb'
+require_relative './peter-parser/behaviours.rb'
