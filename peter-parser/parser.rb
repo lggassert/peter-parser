cbb3f6b407bbc02fce13b8d6422abafb8b36fcf0
Lucas G Gassert <lggassert@gmail.com>
2012-05-03 05:43:20 -0300

    "Parsing is done on instance, not class"

--- peter-parser/parser.rb
+++ peter-parser/parser.rb
@@ -1,30 +1,37 @@
 module PeterParser
     class Parser
-        def self.perform(job=nil)
-            @job = Hash[(job || @default_job).map{|k, v| [k, v]}]
-            prepare()
-            
+        def self.run(job=nil)
+            instance = self.new((job || @default_job || {}))
+            return instance.run()
         end
         
-        def self.prepare()
+        def initialize(job)
+            @job = Hash[job.each{|k, v| [k, v]}]
+        end
+        
+        def run()
+            prepare()
+        end
+        
+        def prepare()
             @job['url'] or raise NoURLOnJob, "Could not find an URL to parse on #{@job}"
             @job['data'] = fetch_data(@job['url'])
             @job['data'] = @job['data'].force_encoding(@page_encoding) if @page_encoding
             @job['doc'] = mount_doc(@job['data'])
-            
+                        
             return nil
         end
         
-        def self.fetch_data(url)
+        def fetch_data(url)
             return RestClient.get(url)
         end
         
-        def self.mount_doc(data)
+        def mount_doc(data)
             return Nokogiri::HTML::Document.new(data)
         end
         
-        def self.call_hook(name)
-            self.send(name) if self.respond_to?(name)
+        def call_hook(name)
+            send(name) if respond_to?(name)
         end
     end
 end
